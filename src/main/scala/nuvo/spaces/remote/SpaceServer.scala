4497aaf3f8ef52122354d56831e494289e01fe75
nuvo-io <io@nuvo.io>
2013-07-31 10:29:17 +0200

    removed filter on StreamManager

--- src/main/scala/nuvo/spaces/remote/SpaceServer.scala
+++ src/main/scala/nuvo/spaces/remote/SpaceServer.scala
@@ -66,7 +66,8 @@ class SpaceServer(val locator: Locator) {
       buf.clear()
       buf.putObject(StreamTuple(0, t))
       buf.flip()
-      streamHandlers.filter(_.p(t)).foreach(h => {
+      // filter(_.p(t)).
+      streamHandlers.foreach(h => if (h.p(t)) {
         h.mp.writeTo(buf, h.cid)
         buf.flip()
       })
